9b35821d36d4f969c3d82eaa31898e2993fb9697
Daniel <daniel.avzaradel@gmail.com>
2025-10-22 18:53:39 +0300

    creating reducer for incrementBy

--- redux.test.ts
+++ redux.test.ts
@@ -17,16 +17,27 @@ const createStore = <State, Action>(initialState: State, reducer: (state: State,
 describe("our redux", () => {
     it("should update state predictably", () => {
         const initialState = { count: 0 };
-        type Actions = { type: "increment" } | { type: "decrement" }
+        type Actions = { type: "increment" } | { type: "decrement" } | { type: "incrementBy", payload: number };
         const reducer = (state: typeof initialState, action: Actions) => {
             switch(action.type) {
                 case 'increment':
                     return { count: state.count + 1 };
                 case 'decrement':
                     return { count: state.count - 1 };
+                case 'incrementBy':
+                    return { count: state.count + action.payload };
                 default:
                     return state; 
             }
         }
+
+        const store = createStore(initialState, reducer);
+        expect(store.getState()).toEqual({count: 0});
+        store.dispatch({type: 'increment'});
+        expect(store.getState()).toEqual({count: 1});
+        store.dispatch({type: 'decrement'});
+        expect(store.getState()).toEqual({count: 0});
+        store.dispatch({type: 'incrementBy', payload: 5});
+        expect(store.getState()).toEqual({count: 5});
     })
 })
