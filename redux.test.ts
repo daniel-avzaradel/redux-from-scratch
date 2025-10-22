978eed9368c3f4a91c1f1ef24086fdabedccf3b7
Daniel <daniel.avzaradel@gmail.com>
2025-10-22 18:48:50 +0300

    creating our store

--- redux.test.ts
+++ redux.test.ts
@@ -1,15 +1,32 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect } from 'vitest';
 
 const createStore = <State, Action>(initialState: State, reducer: (state: State, action: Action) => State) => {
+
+    let state = initialState;
+    const getState = () => state;
+
     return {
-        getState: () => {},
-        dispatch: () => {},
-        subscribe: () => {}
+        getState,
+        dispatch: (action: Action) => {
+            state = reducer(state, action)
+        },
+        subscribe: () => {} 
     }
 }
 
-describe("Our redux", () => {
+describe("our redux", () => {
     it("should update state predictably", () => {
         const initialState = { count: 0 };
+        type Actions = { type: "increment" } | { type: "decrement" }
+        const reducer = (state: typeof initialState, action: Actions) => {
+            switch(action.type) {
+                case 'increment':
+                    return { count: state.count + 1 };
+                case 'decrement':
+                    return { count: state.count - 1 };
+                default:
+                    return state; 
+            }
+        }
     })
 })
