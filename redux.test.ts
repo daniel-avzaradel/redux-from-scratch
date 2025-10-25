0fbb1477da2b2ac252b17706403a4a124fb9acc9
Daniel <daniel.avzaradel@gmail.com>
2025-10-25 18:18:28 +0300

    finished store

--- redux.test.ts
+++ redux.test.ts
@@ -14,3 +14,35 @@ const createStore = <State, Action>(initialState: State, reducer: (state: State,
     }
 }
 
+
+describe("Redux Store", () => {
+    it("Should update state predictably", () => {
+        let initialState = { count: 0 };
+
+        type Actions = { type: "increment" } | { type: "decrement" } | { type: "incrementBy", payload: number }
+
+        const reducer = (state: typeof initialState, action: Actions) => {
+            switch(action.type) {
+                case "increment":
+                    return { count: state.count + 1 };
+                case "decrement":
+                    return { count: state.count - 1 };
+                case "incrementBy":
+                    return { count: state.count + action.payload };
+                default:
+                    return state;
+            }
+        }
+
+        const store = createStore(initialState, reducer);
+        expect(store.getState()).toEqual({ count: 0 });
+
+        store.dispatch({ type: "increment" })
+        expect(store.getState()).toEqual({ count: 1 });
+        store.dispatch({ type: "decrement" })
+        expect(store.getState()).toEqual({ count: 0 });
+
+        store.dispatch({ type: "incrementBy", payload: 500 })
+        expect(store.getState()).toEqual({ count: 500 })
+    })
+})
